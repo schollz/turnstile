ac7476d880799e3cb71ffe53e1c6bc655a35da85
Zack Scholl <zack.scholl@gmail.com>
2021-09-14 13:13:29 -0700

    revert lcm

--- lib/utils.lua
+++ lib/utils.lua
@@ -2,6 +2,12 @@ function lcm(...)
   local function lcm_(x,y)
     local lcm_num=0
     local greater=0
+    local multiplier=1
+    while math.floor(x)~=x or math.floor(y)~=y do
+      x=x*10
+      y=y*10
+      multiplier=multiplier*10
+    end
 
     -- choose the greater number
     if x>y then
@@ -11,22 +17,14 @@ function lcm(...)
     end
 
     while true do
-      local x_r=greater/x-math.floor(greater/x)
-      local y_r=greater/y-math.floor(greater/y)
-      if x_r>0.99 then
-        x_r=0
-      end
-      if y_r>0.99 then
-        y_r=0
-      end
-      if (x_r<0.01 and y_r<0.01) then
+      if ((greater%x==0) and (greater%y==0)) then
         lcm_num=greater
         break
       end
-      greater=greater+0.01
+      greater=greater+1
     end
 
-    return math.floor(lcm_num*10+0.5)/10
+    return lcm_num/multiplier
   end
 
   local res=nil
@@ -43,9 +41,6 @@ function lcm(...)
   return res
 end
 
--- print(lcm(2,3))
--- print(lcm(1.33333,2))
--- print(lcm(1.3333,3.7,4.5))
 
 function current_time()
   return clock.get_beat_sec()*clock.get_beats()
